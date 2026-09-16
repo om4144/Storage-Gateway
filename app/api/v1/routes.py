d4f4d16b774867fff245f7818b63e5563be25100
om4144 <omveersingh2711@gmail.com>
2026-09-16 21:12:12 +0530

    add object load route

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,7 +1,10 @@
+import io
 from fastapi import APIRouter, UploadFile, Depends, HTTPException
+from fastapi.responses import StreamingResponse
 from sqlmodel import Session
 
 from app.services.object_store import ObjectStore
+from app.db.models import ObjectRecord
 from app.db.database import get_session
 
 router = APIRouter()
@@ -24,3 +27,33 @@ async def put_object(file: UploadFile, session: Session = Depends(get_session)):
   # Task 3: return record
   if record:
     return record
+
+@router.get("/load")
+async def get_object(object_id: str, session: Session = Depends(get_session)):
+  # Task 1: Find key using object_id
+  record = session.get(ObjectRecord, object_id)
+
+  # Task 2: Raising exception if object id not found or invalid
+  if not record:
+    raise HTTPException(
+      status_code= 404,
+      detail= f"Bad Request: 'Invalid object id: {object_id}'"
+    )
+
+  # Task 3: upload the data and get back metadata
+  try: 
+    file = await storage.load(key=record.key)
+
+  # Task 4: Raising error in case of failure
+  except Exception as e:
+    raise HTTPException(
+      status_code= 500,
+      detail= "Failed to load data due to 'Internal Server Error'"
+    ) from e
+
+  # Task 5: return file
+  return StreamingResponse(
+    file,
+    media_type=record.content_type,
+    headers={"Content-Disposition": f'attachment; filename="{record.filename}"'}
+  )
